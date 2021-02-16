0c5725c54af378008f56c4d1432994299a42a33e
AkbarZul <akbar.zul73@gmail.com>
2021-02-16 13:48:33 +0700

    add product fix

--- src/components/MyProfile/CustomerProfile.js
+++ src/components/MyProfile/CustomerProfile.js
@@ -1,13 +1,39 @@
 import React, { useState } from "react";
 // import { useSelector, useDispatch } from "react-redux";
 import { Form, Row, Col } from "react-bootstrap";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { API } from "../../utility/Auth";
+import axios from "axios";
 // import Sidebar from "../SidebarProfile/Sidebar";
 import "./style.css";
 
 const CustomerProfile = () => {
   const full_name = useSelector((state) => state.auth.data.full_name);
-  const email = useSelector((state) => state.auth.data.email)
+  const email = useSelector((state) => state.auth.data.email);
+  const token = useSelector((state) => state.auth.data.token);
+  const [name, setName] = useState("");
+  const [mail, setMail] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const updateProfile = {
+      full_name: name,
+      email: mail,
+    };
+
+    await axios
+      .put(API + `/auth/profile`, updateProfile, {
+        headers: {
+          "x-access-token": "Bearer " + token,
+        },
+      })
+      .then(({ data }) => {
+        console.log("update profile", data);
+      })
+      .catch((err) => {
+        console.log("error update profile", err.response);
+      });
+  };
   // console.log("ini fullname dari profile", full_name)
   // console.log("ini email dari profile", email)
   return (
@@ -23,7 +49,12 @@ const CustomerProfile = () => {
               Name
             </Form.Label>
             <Col>
-              <Form.Control type="text" placeholder={full_name} />
+              <Form.Control
+                type="text"
+                placeholder={full_name}
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
             </Col>
           </Form.Group>
           <Form.Group as={Row} controlId="formHorizontalPassword">
@@ -31,7 +62,12 @@ const CustomerProfile = () => {
               Email
             </Form.Label>
             <Col sm={9}>
-              <Form.Control type="email" placeholder={email} />
+              <Form.Control
+                type="email"
+                placeholder={email}
+                value={mail}
+                onChange={(e) => setMail(e.target.value)}
+              />
             </Col>
           </Form.Group>
           <Form.Group as={Row} controlId="formHorizontalPassword">
@@ -45,7 +81,7 @@ const CustomerProfile = () => {
           <Form.Group as={Row} controlId="formHorizontalPassword">
             <Form.Label column sm={3} className="mt-n5"></Form.Label>
             <Col sm={9}>
-              <button className="save">
+              <button className="save" onClick={handleSubmit}>
                 <div className="btn-login-nav ">Save</div>
               </button>
             </Col>
